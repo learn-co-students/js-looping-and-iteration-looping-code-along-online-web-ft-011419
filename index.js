e96905acffcf909a4e9d2e60df21fd1cf9680795
Luis Zabala <luis.zabala076@gmail.com>
2019-04-22 23:58:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const badges = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges (badges) {
+  for (let i = 0; i < badges.length; i++) {
+    console.log('Welcome ${badges[i]}, You are employee #${i + 1).');
+  }
+  return badges;
+}
+
+function tailsNeverFails () {
+  let i = 0;
+
+  while (Math.random() >= 0.5) {
+    i++;
+  }
+
+  return `You got ${i} tails in a row!`;
+}
