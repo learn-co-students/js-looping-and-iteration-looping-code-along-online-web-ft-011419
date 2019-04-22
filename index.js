efa875b1a2f19f14420a93f3f091b591409213a5
Luis Zabala <luis.zabala076@gmail.com>
2019-04-22 23:59:57 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function printBadges (badges) {
 }
 
 function tailsNeverFails () {
+// counter [i] = 0
   let i = 0;
 
+// while random num is equal to or greater than .5 increase the counter.
   while (Math.random() >= 0.5) {
     i++;
   }
-
+// return the counter total
   return `You got ${i} tails in a row!`;
 }
