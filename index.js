5b2821469359b47ea5e8e567a94d3f635b12c787
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-05-09 11:52:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+  let greetings = [];
+  for (let i = 0; i < names.length; i++) {
+    greetings[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+
+  return greetings;
+}
+
+function countdown(i){
+
+  while(i>=0){
+    console.log(i);
+    i--;
+  }
+
+}
